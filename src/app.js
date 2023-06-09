4436ed885bf3748a719f53ef11177c280c7e0bf5
Angelo Velasquez <angeloquez1782@hotmail.com>
2023-06-09 09:07:14 -0500

    he hecho un set de puerto y funciona como condicional

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.listen(3000, () => {
+//settings
+app.set("port", process.env.PORT || 3000);
+
+app.listen(app.get("port"), () => {
     console.log("Server on port 3000");
 });
