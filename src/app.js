b8961b87335a8a382176275e289ac962f9d345c8
Angelo Velasquez <angeloquez1782@hotmail.com>
2023-06-10 13:11:56 -0500

    Actualizando algunos cambios

--- src/app.js
+++ src/app.js
@@ -24,6 +24,7 @@ app.use(myConnection(mysql2, {
     port: 3306,
     database: "crudnodejs"
 }, "single"));
+app.use(express.urlencoded({extended: false}));
 
 // routes
 app.use("/", customerRoutes);
