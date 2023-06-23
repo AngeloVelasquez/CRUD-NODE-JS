df645edcc7f40161109364ee0f57919b4e853866
Angelo Velasquez <angeloquez1782@hotmail.com>
2023-06-23 07:13:31 -0500

    Agregue algunos comentarios y ordene un poco el codigo, intente modificar un poco el controller que se encarga de hacer delete pero sigue sin funcionar usando el metodo delete

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -1,17 +1,30 @@
-const controller = {};
+const controller = {}; // esto hace que controlador se defina como objeto
 
 controller.list = (req, res) => {
     req.getConnection((err, conn) => {
-        conn.query("SELECT * FROM customer", (err, customers) => {
-            if(err) {
-                res.json(err);
-            }
-            res.render("customers", {
-                data: customers
-            });
-        });
+      conn.query("SELECT * FROM customer", (err, customers) => { // hago un query de conexion con la base de datos y selecciono la tabla que usare
+        if (err) {
+          res.json(err); // si no me puede mostrar la tabla entonces enviara un json que notifique el error
+        } 
+        else {
+          const renderedData = customers.map((customer, i) => { // con customers.map puedo transformar cada elemento del array
+            return {
+              i: i + 1, //defino lo que quiero que se muestre en la tabla
+              nombre: customer.nombre,
+              dirección: customer.dirección,
+              telefono: customer.telefono,
+              id: customer.id
+            };
+          });
+
+          res.render("customers", {
+            data: renderedData
+          });
+        }
+      });
     });
-};
+  };
+  
 
 controller.save = (req, res) => {
     const data = req.body;
@@ -19,7 +32,7 @@ controller.save = (req, res) => {
     req.getConnection((err, conn) => {
         conn.query("INSERT INTO customer set ?", [data], (err, customer) => {
             res.redirect("/");
-        });
+        })
     })
 }
 
@@ -29,10 +42,10 @@ controller.edit = (req, res) => {
         conn.query("SELECT * FROM customer WHERE id = ?", [id], (err, customer) => {
             res.render("customer_edit", {
                 data: customer[0]
-            });
-        });
-    });
-};
+            })
+        })
+    })
+}
 
 controller.update = (req, res) => {
     const { id } = req.params;
@@ -40,54 +53,21 @@ controller.update = (req, res) => {
     req.getConnection((err, conn) => {
         conn.query("UPDATE customer set ? WHERE id = ?", [newCustomer, id], (err, rows) => {
             res.redirect("/");
-        });
-    });
-};
+        })
+    })
+}
 
 controller.delete = (req, res) => {
     const { id } = req.params;
     req.getConnection((err, conn) => {
         conn.query("DELETE FROM customer WHERE id = ?", [id], (err, rows) => {
-            res.redirect("/");
-        });
-    });
-};
-
-controller.show = (req, res) => {
-    res.render ("customers")
-    data: customers
-    req.getConnection((err, conn) => {
-        conn.query("SELECT * FROM customer", (customers) => {
-            if (data) {
-                for (var i = 0; i < data.length; i++) {
-                    const id = i + 1
-                    const nombre = data[i].nombre
-                    const dirección = data[i].dirección
-                    const telefono = data[i].telefono
-
-                    const  div = document.createElement("div");
-                    div1.appendChild(id);
-                    div2.appendChild(nombre);
-                    div3.appendChild(dirección);
-                    div4.appendChild(telefono);
-                }
-
-                function traerdatos() {
-                    fetch("/show")
-                    .then((res) => res.json())
-                    .then((data) => {
-                        controller.show;
-                    })
-                }
+            if (err) {
+                res.status(500).json({ error: "Error al eliminar"})
+            } else {
+                res.redirect("/")
             }
-            
-        });
-    });
+        })
+    })
 }
-
-
-    
-
-
 
 module.exports = controller;
